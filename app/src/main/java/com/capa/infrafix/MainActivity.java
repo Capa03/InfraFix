bfb9431cb189f865470b5a072116cddaf3d35b1c
DESKTOP-TV3U1VB\Capa <capaseitadavid@gmail.com>
2022-12-12 18:14:29 +0000

    BottomNavBar Implemention and Blank Fragments

--- app/src/main/java/com/capa/infrafix/MainActivity.java
+++ app/src/main/java/com/capa/infrafix/MainActivity.java
@@ -1,14 +1,30 @@
 package com.capa.infrafix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.capa.infrafix.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        this.bottomMenu();
+    }
+
+    private void bottomMenu(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(binding.fragmentContainerView.getId());
+        NavigationUI.setupWithNavController(binding.bottomNavBar, navHostFragment.getNavController());
     }
 }
